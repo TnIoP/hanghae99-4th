033f21c0fe0172487d933d1c2ad09ef487000ee6
TnIoP <pointblank94@naver.com>
2021-11-29 19:08:40 +0900

    항해99 4기 5주차 Node.js 심화 주차 개인 과제 (순한맛 Ver.) 구현 완료

--- src/index.js
+++ src/index.js
@@ -1,10 +1,97 @@
-class Site {}
+class Site {
+    constructor() {
+        this.boards = [];
+    }
 
-class Board {}
+    addBoard(board) {
+        for (let i = 0; i < this.boards.length; i++) {
+            if (this.boards[i].name === board.name) throw new Error('Same BoardName.');
+        }
+        board.registerCheck = true;
+        this.boards.push(board);
+    }
 
-class Article {}
+    findBoardByName(boardName) {
+        for (let i = 0; i < this.boards.length; i++) {
+            if (this.boards[i].name === boardName) {
+                return this.boards[i];
+            }
+        }
+    }
+}
 
-class Comment {}
+class Board {
+    constructor(boardName) {
+        this.article = [];
+        if (boardName === '' || boardName === null) throw new Error('Invalid BoardName.');
+        this.name = boardName;
+        this.registerCheck = false;
+    }
+
+    publish(article) {
+        var date = new Date();
+
+        if (this.registerCheck === false) throw new Error('not register Site.');
+        article.id = this.name + '-' + Math.floor(Math.random() * 10);
+        article.createdDate = date.toISOString();
+        article.registerCheck = true;
+        this.article.push(article);
+    }
+
+    getAllArticles() {
+        return this.article;
+    }
+}
+
+class Article {
+    constructor(jsonPost) {
+        this.comment = [];
+
+        if (
+            jsonPost.subject === '' ||
+            jsonPost.subject === null ||
+            jsonPost.content === '' ||
+            jsonPost.content === null ||
+            jsonPost.author === '' ||
+            jsonPost.author === null
+        )
+            throw new Error('Invalid Input.');
+
+        this.subject = jsonPost.subject;
+        this.content = jsonPost.content;
+        this.author = jsonPost.author;
+        this.registerCheck = false;
+        this.id = '';
+        this.createdDate = '';
+    }
+
+    reply(comment) {
+        var date = new Date();
+
+        if (this.registerCheck === false) throw new Error('not register Article.');
+        comment.createdDate = date.toISOString();
+        this.comment.push(comment);
+    }
+
+    getAllComments() {
+        return this.comment;
+    }
+}
+
+class Comment {
+    constructor(jsonComment) {
+        if (
+            jsonComment.content === '' ||
+            jsonComment.content === null ||
+            jsonComment.author === '' ||
+            jsonComment.author === null
+        )
+            throw new Error('Invalid Input.');
+        this.content = jsonComment.content;
+        this.author = jsonComment.author;
+        this.createdDate = '';
+    }
+}
 
 module.exports = {
     Site,
