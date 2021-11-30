4bfaef4e9c569653f0cf39d02f6b6b682c157cff
TnIoP <pointblank94@naver.com>
2021-11-30 13:58:10 +0900

    항해99 4기 5주차 Node.js 심화 주차 개인 과제 (매운맛 Ver.) 구현 완료

--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
-class Sensor {}
+class Sensor {
+    constructor(id) {
+        this.id = id;
+        this.powerStatus = 'off';
+        this.status = 'waiting';
+        this.reportingInterval = 10000;
+    }
 
-class IotServer {}
+    turn(status) {
+        if (this.powerStatus === 'off' && status === 'on') {
+            this.powerStatus = 'on';
+            this.status = 'idle';
+
+            setTimeout(() => {
+                this.status = 'sensingDistance';
+                setTimeout(() => {
+                    this.status = 'reportingData';
+                    setTimeout(() => {
+                        this.status = 'idle';
+                    }, 1000);
+                }, 500);
+            }, this.reportingInterval);
+        } else if (status === 'off') {
+            this.powerStatus = 'off';
+        } else throw new Error('Status not changed.');
+    }
+}
+
+class IotServer {
+    constructor() {
+        this.sensor = [];
+    }
+
+    start(sensorArr) {
+        this.sensor.push(sensorArr[0]); // 배열안에 json데이터가 와서 0번째를 push
+    }
+
+    publish(jsonData) {
+        for (let i = 0; i < this.sensor.length; i++) {
+            if (this.sensor[i].id === jsonData.deviceId) {
+                if (this.sensor[i].powerStatus === 'on' && jsonData.actionId === 'CHANGE_REPORTING_INTERVAL') {
+                    this.sensor[i].reportingInterval = jsonData.payload;
+                }
+            }
+        }
+    }
+}
 
 module.exports = {
     Sensor,
