a6cb7c806ecdbd454730f832f6562f826adc269e
TnIoP <pointblank94@naver.com>
2021-12-07 11:12:39 +0900

    comment 파트 구현 완료

--- routers/comment.js
+++ routers/comment.js
@@ -1,10 +1,54 @@
-const express = require('express')
-const router = express.Router()
-const url = require('url')
-const Comments = require('../schemas/comments')
-const authMiddleware = require('../middlewares/auth-middleware')
+const express = require('express');
+const router = express.Router();
+const url = require('url');
+const Comments = require('../schemas/comments');
+const authMiddleware = require('../middlewares/auth-middleware');
 
+// 댓글 목록 조회
+router.get('/comments/:postId', authMiddleware, async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+    let comments = await Comments.find({ postId }).sort('commentId').lean();
+    res.json({ comments: comments });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
 
+// 댓글 작성
+router.post('/comments/:commentId', authMiddleware, async (req, res) => {
+  const { postId, content } = req.body;
+  const { userId } = res.locals.user;
+  const recentComment = await Comments.find().sort('-commentId').limit(1);
+  let commentId = 1;
+  if (recentComment.length != 0) {
+    commentId = recentComment[0]['commentId'] + 1;
+  }
 
+  await Comments.create({ postId, userId, commentId, content });
+  res.send({ result: 'success' });
+});
 
-module.exports = router 
+// 댓글 수정 시 해당 댓글 하나 가져오기
+router.get('/comments/:commentId', async (req, res) => {
+  const { commentId } = req.params;
+  comment = await Comments.findOne({ commentId });
+  res.json({ detail: comment });
+});
+
+// 댓글 수정
+router.patch('/comments/:commentId', async (req, res) => {
+  const { content, commentId } = req.body;
+  await Comments.updateOne({ commentId }, { $set: { content } });
+  res.send({ result: 'success' });
+});
+
+// 댓글 삭제
+router.delete('/comments/:commentId', async (req, res) => {
+  const { commentId } = req.params;
+  await Comments.deleteOne({ commentId });
+  res.send({ result: 'success' });
+});
+
+module.exports = router;
