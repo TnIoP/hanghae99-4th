2ab31dbc5c77ae6c1d94777a4b4ebc9f1d962353
TnIoP <pointblank94@naver.com>
2021-12-08 10:21:45 +0900

    Revert "comments 스키마에 userName추가"
    This reverts commit cc078c812d21b761fad500119135f9bf902dfe64.

--- schemas/comments.js
+++ schemas/comments.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const CommentsSchema = new mongoose.Schema({
   postId: Number,
   userId: Number,
-  userName: String,
   commentId: Number,
   content: String,
 });
